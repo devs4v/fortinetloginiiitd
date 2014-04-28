896edace303da162d84065eb4198838eaed84556
Prakhar Shukla <prafiles@yahoo.com>
2014-04-28 09:59:44 +0530

    new windows form C# app for the same
    natively coded with fewer dependencies for better functionality and
    experience (maybe even performance)

---
+++ windows/FortinetAutoLoginIIITD/FortinetAutoLoginIIITD/MainWindow.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FortinetAutoLoginIIITD
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.btn_Tray = new System.Windows.Forms.Button();
+            this.chk_DontAskAgain = new System.Windows.Forms.CheckBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.notification = new System.Windows.Forms.NotifyIcon(this.components);
+            this.txt_Password = new System.Windows.Forms.MaskedTextBox();
+            this.btn_About = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Location = new System.Drawing.Point(78, 15);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(151, 20);
+            this.txt_Username.TabIndex = 0;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Location = new System.Drawing.Point(12, 18);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(60, 13);
+            this.lbl_Username.TabIndex = 2;
+            this.lbl_Username.Text = "User Name";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(12, 46);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(53, 13);
+            this.lbl_Password.TabIndex = 3;
+            this.lbl_Password.Text = "Password";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(248, 13);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 23);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // btn_Tray
+            // 
+            this.btn_Tray.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Tray.Location = new System.Drawing.Point(248, 41);
+            this.btn_Tray.Name = "btn_Tray";
+            this.btn_Tray.Size = new System.Drawing.Size(75, 23);
+            this.btn_Tray.TabIndex = 5;
+            this.btn_Tray.Text = "Hide to Tray";
+            this.btn_Tray.UseVisualStyleBackColor = true;
+            this.btn_Tray.Click += new System.EventHandler(this.btn_Tray_Click);
+            // 
+            // chk_DontAskAgain
+            // 
+            this.chk_DontAskAgain.AutoSize = true;
+            this.chk_DontAskAgain.Location = new System.Drawing.Point(15, 74);
+            this.chk_DontAskAgain.Name = "chk_DontAskAgain";
+            this.chk_DontAskAgain.Size = new System.Drawing.Size(218, 17);
+            this.chk_DontAskAgain.TabIndex = 6;
+            this.chk_DontAskAgain.Text = "Automatically login and hide this window.";
+            this.chk_DontAskAgain.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(-1, 99);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(335, 13);
+            this.progressBar.TabIndex = 7;
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Location = new System.Drawing.Point(2, 115);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(137, 13);
+            this.lbl_Status.TabIndex = 8;
+            this.lbl_Status.Text = "Status : Starting Application";
+            // 
+            // notification
+            // 
+            this.notification.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notification.BalloonTipText = "Auto Login is hiding. Click icon to restore.";
+            this.notification.BalloonTipTitle = "Auto Login @ IIITD - Fortinet";
+            this.notification.Icon = ((System.Drawing.Icon)(resources.GetObject("notification.Icon")));
+            this.notification.Text = "Auto Login is hiding.";
+            this.notification.Visible = true;
+            this.notification.Click += new System.EventHandler(this.restoreWindow);
+            this.notification.DoubleClick += new System.EventHandler(this.restoreWindow);
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(78, 43);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(151, 20);
+            this.txt_Password.TabIndex = 9;
+            this.txt_Password.UseSystemPasswordChar = true;
+            // 
+            // btn_About
+            // 
+            this.btn_About.Location = new System.Drawing.Point(248, 70);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(75, 23);
+            this.btn_About.TabIndex = 10;
+            this.btn_About.Text = "About";
+            this.btn_About.UseVisualStyleBackColor = true;
+            this.btn_About.Click += new System.EventHandler(this.btn_About_Click);
+            // 
+            // MainWindow
+            // 
+            this.AcceptButton = this.btn_Login;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(333, 134);
+            this.Controls.Add(this.btn_About);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.lbl_Status);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.chk_DontAskAgain);
+            this.Controls.Add(this.btn_Tray);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.txt_Username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Auto Login @ IIITD - Fortinet";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_Tray;
+        private System.Windows.Forms.CheckBox chk_DontAskAgain;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.NotifyIcon notification;
+        private System.Windows.Forms.MaskedTextBox txt_Password;
+        private System.Windows.Forms.Button btn_About;
+    }
+}
+
---
+++ windows/FortinetAutoLoginIIITD/FortinetAutoLoginIIITD/MainWindow.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace FortinetAutoLoginIIITD
+{
+    public partial class MainWindow : Form
+    {
+        bool loginStatus;
+        public MainWindow()
+        {
+            InitializeComponent();
+            loginStatus = false;
+        }
+
+        private void btn_Tray_Click(object sender, EventArgs e)
+        {
+            notification.Visible = true;
+            notification.ShowBalloonTip(500);
+            this.Hide();
+        }
+
+        private void restoreWindow(object sender, EventArgs e)
+        {
+            this.Show();
+            notification.Visible = false;
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            chk_DontAskAgain.Checked = Properties.Settings.Default.AutoLogin;
+            txt_Username.Text = Properties.Settings.Default.Username;
+            txt_Password.Text = Properties.Settings.Default.Password;
+            backgroundWorker();
+            if (chk_DontAskAgain.Checked)
+            {
+                performLogin(txt_Username.Text, txt_Password.Text);
+                btn_Login.Text = "Logout";
+                loginStatus = true;
+            }
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Username = txt_Username.Text;
+            Properties.Settings.Default.Password = txt_Password.Text;
+            Properties.Settings.Default.AutoLogin = chk_DontAskAgain.Checked;
+            Properties.Settings.Default.Save();
+            performLogin(txt_Username.Text, txt_Password.Text);
+            loginStatus = true;
+            if (chk_DontAskAgain.Checked)
+            {
+                notification.Visible = true;
+                notification.ShowBalloonTip(500);
+                this.Hide();
+            }
+            if (loginStatus)
+            {
+                lbl_Status.Text = "Performing Login";
+                performLogin(txt_Username.Text, txt_Password.Text);
+                btn_Login.Text = "Logout";
+            }
+            else
+            {
+                lbl_Status.Text = "Performing Logout";
+                performLogout();
+                btn_Login.Text = "Login";
+            }
+        }
+
+        private void performLogin(string username, string password)
+        {
+
+        }
+
+        private int checkConnectivity()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            try
+            {
+                Stream data = client.OpenRead("http://www.wikipedia.org/");
+                StreamReader reader = new StreamReader(data);
+                string s = reader.ReadToEnd();
+                if (s.IndexOf("<title>Wikipedia</title>") > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+        private void performLogout()
+        {
+
+        }
+
+        private void btn_About_Click(object sender, EventArgs e)
+        {
+            AboutBox about = new AboutBox();
+            about.Show();
+        }
+
+        private int backgroundWorker()
+        {
+            progressBar.Value = 0;
+            int value = checkConnectivity();
+            switch (value)
+            {
+                case 1:
+                    if (loginStatus == false)
+                    {
+                        lbl_Status.Text = "Connected to direct internet!";
+                        progressBar.Value = 100;
+                        btn_Login.Enabled = false;
+                    }
+                    else
+                    {
+                        lbl_Status.Text = "Connected to direct internet!";
+                        progressBar.Value = 100;
+                        btn_Login.Enabled = false;
+                    }
+                    break;
+                case -1:
+                    loginStatus = false;
+                    lbl_Status.Text = "Check Connecitivity to Internet";
+                    progressBar.Value = 33;
+                    btn_Login.Enabled = false;
+                    break;
+                case 0:
+                    progressBar.Value = 33;
+                    btn_Login.Enabled = true;
+                    lbl_Status.Text = "Login to proceed.";
+                    break;
+            }
+            return value;
+        }
+    }
+}
