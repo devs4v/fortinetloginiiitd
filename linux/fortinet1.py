2eb1d4b355439acc7e07b7af8cc67d3bb87e80d1
Prakhar Shukla <prafiles@yahoo.com>
2014-04-28 05:56:22 +0530

    Lots of changes

---
+++ linux/fortinet1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import urllib
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
+from time import sleep
+import gc
+import ctypes
+import time
+import sys
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+############Settings
+#Fill in your username here
+
